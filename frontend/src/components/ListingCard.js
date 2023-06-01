48c7ae1b55091b16122aaf6f963d997be8eb62d7
Cristian Ciacu <d.c.ciacu@student.tudelft.nl>
2023-06-01 15:44:36 +0200

    add padding after icons and add text room or roooms.

--- frontend/src/components/ListingCard.js
+++ frontend/src/components/ListingCard.js
@@ -25,13 +25,13 @@ export default function ListingCard({ element, index }) {
                         {/* Price */}
                         {element['price_min'] !== element['price_max'] && (
                             <p id="price">
-                                <FontAwesomeIcon icon={faMoneyBill} /> 
+                                <FontAwesomeIcon icon={faMoneyBill} className="pe-2" /> 
                                 {element['price_min']} - {element['price_max']}
                             </p>
                         )}
                         {element['price_min'] === element['price_max'] && (
                             <p id="price">
-                                <FontAwesomeIcon icon={faMoneyBill} className="pe-1" />
+                                <FontAwesomeIcon icon={faMoneyBill} className="pe-2" />
                                 {element['price_min']}
                             </p>
                         )}
@@ -39,13 +39,13 @@ export default function ListingCard({ element, index }) {
                         {/* Area */}
                         {element['area_min'] !== element['area_max'] && (
                             <p id="surface_area" className="pe-3 pb-2">
-                                <FontAwesomeIcon icon={faChartArea} className="pe-1" />
+                                <FontAwesomeIcon icon={faChartArea} className="pe-2" />
                                 {element['area_min']} m² - {element['area_max']} m²
                             </p>
                         )}
                         {element['area_min'] === element['area_max'] && (
                             <p id="surface_area" className="pe-3 pb-2">
-                                <FontAwesomeIcon icon={faChartArea} className="pe-1" /> 
+                                <FontAwesomeIcon icon={faChartArea} className="pe-2" /> 
                                 {element['area_min']} m²
                             </p>
                         )}
@@ -53,14 +53,14 @@ export default function ListingCard({ element, index }) {
                         {/* Number of rooms */}
                         {element['no_of_rooms_min'] !== element['no_of_rooms_max'] && (
                             <p id="number_of_rooms" className="pe-3 pb-2">
-                                <FontAwesomeIcon icon={faPeopleRoof} className="pe-1" />
-                                {element['no_of_rooms_min'] == 0 ? "Not available" : element['no_of_rooms_min']} - {element['no_of_rooms_max']}
+                                <FontAwesomeIcon icon={faPeopleRoof} className="pe-2" />
+                                {element['no_of_rooms_min'] == 0 ? "Not available" : element['no_of_rooms_min']} - {element['no_of_rooms_max']} rooms
                             </p>
                         )}
                         {element['no_of_rooms_min'] === element['no_of_rooms_max'] && (
                             <p id="number_of_rooms" className="pe-3 pb-2">
-                                <FontAwesomeIcon icon={faPeopleRoof} className="pe-1" />
-                                {element['no_of_rooms_min'] == 0 ? "Not available" : element['no_of_rooms_min']}
+                                <FontAwesomeIcon icon={faPeopleRoof} className="pe-2" />
+                                {element['no_of_rooms_min'] == 0 ? "Not available" : element['no_of_rooms_min'] === 1 ? `${element['no_of_rooms_min']} room` : `${element['no_of_rooms_min']} rooms`}
                             </p>
                         )}
 
